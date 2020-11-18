0cbec7497e9c95a7aac89064c6f2e97a7aa7f115
hanjungwoo <ajtwlswjddnv1102@gmail.com>
2020-11-18 09:26:16 +0900

    feat:script 변경

--- src/execute.sh
+++ src/execute.sh
@@ -1,1 +1,11 @@
-sudo python execute_mn.py 40 test.txt test_result.txt
+SRC_FILENAME=test.txt
+DST_FILENAME=1_result.txt
+SENDER_LOG_FILENAME=sender_log.txt
+RECEIVER_LOG_FILENAME=receiver_log.txt
+
+
+echo 'remove the log and dst file'
+rm -rf $SENDER_LOG_FILENAME $RECEIVER_LOG_FILENAME $DST_FILENAME
+
+echo 'excecute'
+sudo python execute_mn.py 40 $SRC_FILENAME $DST_FILENAME
